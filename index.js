cf21096812a0d5049c33f12a14aa622eb84e4225
Rafael García <rafael.gm2@gmail.com>
2021-06-17 22:15:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//PWCheckout.Bind('tokenCreated', OnTokenReceived);
+PWCheckout.AddActionButton('buttonId1');
+PWCheckout.SetProperties({
+    name: 'Demo Test.',
+    email: 'gpigni@pagosweb.com.uy',
+    //"image": "http://pepito.com/images/logocheckout.png", "button-label": "Pagar #monto#",
+    description: 'Checkout Creditel Test.',
+    currency: 'DOP',
+    amount: '100',
+    lang: 'ESP',
+    form_id: 'shoppingcart_form',
+    checkout_card: 1,
+    autoSubmit: 'false',
+    empty: 'true'
+});
+// function OnTokenReceived(token) {
+//   alert(token.TokenId);
+//   document.getElementById('PWTokenAux').value = token.TokenId;
+// }
+// document
+//   .getElementById('btnCheckout')
+//   .addEventListener('click', function (event) {
+//     event.preventDefault();
+//     myFunction();
+//   });
